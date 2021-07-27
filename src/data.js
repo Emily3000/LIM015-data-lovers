b3b1319d9a3bfab3b50712a17d39a429955bc9c6
Emily3000 <emily.ferpanebra@gmail.com>
2021-07-27 00:36:48 -0500

    "Test al 100%"

--- src/data.js
+++ src/data.js
@@ -1,8 +1,124 @@
-export const filterData = (data, conditions) => {
+/*export const filterData = (data, conditions) => {
     let resultFilter = data.filter(
         (pelicula) => pelicula["director"] == conditions
       );
       return resultFilter;
-  };
- 
-       
+  };*/
+  import data from "./data/ghibli/ghibli.js";
+
+  const filmsdata = data["films"];
+  const filmshtml = document.getElementById("peliculasData");
+  
+  let htmls = "";
+       const peliculasGhibli = {
+        peliculasDefault:()=>{
+            filmsdata.forEach((pelicula) => {
+                let auxPelicula = `<div class="films"> 
+                    <img class="poster" src= "${pelicula["poster"]}">
+                        <div class="contenido">
+                          <h3 class="title">${pelicula["title"]}</h3>
+                          <p class="description">${pelicula["description"]}</p>
+                          <p class="director">Director: ${pelicula["director"]}</p>
+                          <p class="producer">Producer: ${pelicula["producer"]}</p>
+                          <p class="date">Date: ${pelicula["release_date"]}</p>
+                          <p class="score">Score: ${pelicula["rt_score"]}</p>
+                        </div>
+                </div>`;
+            
+                htmls += auxPelicula;
+              });
+            
+              filmshtml.innerHTML = htmls;
+            },
+      
+
+        peliculasPorSeleccion:(peliculas)=> {
+        htmls = "";
+        peliculas.forEach((pelicula) => {
+          let auxPelicula = `<div class="films"> 
+                      <img class="poster" src= "${pelicula["poster"]}">
+                      <div class="contenido">
+                        <h3 class="title">${pelicula["title"]}</h3>
+                        <p class="description">${pelicula["description"]}</p>
+                        <p class="director">Director: ${pelicula["director"]}</p>
+                        <p class="producer">Producer: ${pelicula["producer"]}</p>
+                        <p class="date">Date: ${pelicula["release_date"]}</p>
+                        <p class="score">Score: ${pelicula["rt_score"]}</p>
+                      </div>
+              </div>`;
+      
+          htmls += auxPelicula;
+        });
+        filmshtml.innerHTML = htmls;
+      },
+      filterData:(data, conditions)=>{
+        
+        let resultFilter = data.filter(
+            (pelicula) => pelicula["director"] == conditions
+            );
+            return resultFilter;
+    },
+      orderData:(data, conditions)=>{
+        let resultOrder = [];
+            if (conditions == 1) {
+        resultOrder = data.sort(function (p1, p2) {
+      if (p1["title"] > p2["title"]) {
+        return 1;
+      }
+      if (p1["title"] < p2["title"]) {
+        return -1;
+      }
+      return 0;
+    });
+  } else if (conditions == 2) {
+    resultOrder = data.sort(function (p1, p2) {
+      if (p1["title"] < p2["title"]) {
+        return 1;
+      }
+      if (p1["title"] > p2["title"]) {
+        return -1;
+      }
+      return 0;
+    });
+  } else if (conditions == 3) {
+    resultOrder = data.sort(function (p1, p2) {
+      if (p1["release_date"] > p2["release_date"]) {
+        return 1;
+      }
+      if (p1["release_date"] < p2["release_date"]) {
+        return -1;
+      }
+      return 0;
+    });
+  } else if (conditions == 4) {
+    resultOrder = data.sort(function (p1, p2) {
+      if (p1["release_date"] < p2["release_date"]) {
+        return 1;
+      }
+      if (p1["release_date"] > p2["release_date"]) {
+        return -1;
+      }
+      return 0;
+        });
+    }
+    else if(conditions==6){
+      let resultOrder = data.sort((a,b)=>{
+        return a.rt_score-b.rt_score;
+      });
+      return resultOrder;
+    }
+    else if(conditions==5){
+      let resultOrder = data.sort((a,b)=>{
+        return a.rt_score-b.rt_score;
+      })
+      return resultOrder.reverse();
+    }
+    return resultOrder;
+
+      }
+
+    }
+    
+    export default peliculasGhibli;
+    
+
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,7 +1,27 @@
-import {filterData} from '../src/data';
+import peliculasGhibli from '../src/data.js';
 
 describe("filtrar data por directores",() =>{
   it("filterData es una función", ()=>{
-    expect(typeof filterData).toBe("function");
+    expect(typeof peliculasGhibli.filterData).toBe("function");
   });
-});
+})
+describe("filtrar data por titulo",() =>{
+  it("filterData es una función", ()=>{
+    expect(typeof peliculasGhibli.filterData).toBe("function");
+  });
+})
+describe("Ordenar por alfabeto",()=>{
+  it("orderData es una función", ()=>{
+    expect(typeof peliculasGhibli.orderData).toBe("function");
+  });
+})
+describe("Ordenar por año",()=>{
+  it("orderData es una función",()=>{
+    expect(typeof peliculasGhibli.orderData).toBe("function");
+  });
+})
+describe("Ordenar por popularidad",()=>{
+  it("orderData es una función",()=>{
+    expect(typeof peliculasGhibli.orderData).toBe("function");
+  });
+})
